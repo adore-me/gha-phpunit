4519f94edae140af8d25cca98758e07f29cde18a
mihai <mih.gogu@gmail.com>
2023-08-30 14:50:42 +0300

    Run migrations for symfony fix

--- phpunit.sh
+++ phpunit.sh
@@ -100,7 +100,6 @@ if [ "$INPUT_ENABLE_MYSQL" == "true" ]; then
   echo -e "${BL}Info:${NC} Bootstrap fresh DB"
   if [ "$INPUT_RUN_MIGRATIONS" == "true" ]; then
     echo -e "${BL}Info:${NC} Running migrations"
-    docker exec nginx-fpm-alpine bash -c "php artisan migrate:fresh -n --force"
     if [ "$IS_SYMFONY" == "true" ]; then
       docker exec nginx-fpm-alpine bash -c "bin/console tools:database:refresh"
     else
