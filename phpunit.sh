2e17bda165e62c693ab15626188be719ee2a905e
mihai <mih.gogu@gmail.com>
2023-08-31 12:22:04 +0300

    Added seeds for symfony

--- phpunit.sh
+++ phpunit.sh
@@ -101,7 +101,7 @@ if [ "$INPUT_ENABLE_MYSQL" == "true" ]; then
   echo -e "${BL}Info:${NC} Bootstrap fresh DB"
   if [ "$INPUT_RUN_MIGRATIONS" == "true" ]; then
     echo -e "${BL}Info:${NC} Running migrations"
-    if [ $IS_SYMFONY == true ]; then
+    if [ "$IS_SYMFONY" == "true" ]; then
       docker exec nginx-fpm-alpine bash -c "bin/console tools:database:refresh"
     else
       docker exec nginx-fpm-alpine bash -c "php artisan migrate:fresh -n --force"
@@ -118,7 +118,11 @@ if [ "$INPUT_ENABLE_MYSQL" == "true" ]; then
 
   if [ "$INPUT_RUN_SEEDS" == "true" ]; then
     echo -e "${BL}Info:${NC} Running seeds"
-    docker exec nginx-fpm-alpine bash -c "php artisan db:seed --force"
+    if [ "$IS_SYMFONY" == "true" ]; then
+      docker exec nginx-fpm-alpine bash -c "bin/console tools:database:seed"
+    else
+      docker exec nginx-fpm-alpine bash -c "php artisan db:seed --force"
+    fi
   fi
   SEEDS_EXIT_CODE=$?
   if [ "$SEEDS_EXIT_CODE" != "0" ]; then
